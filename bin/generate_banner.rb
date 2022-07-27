da2e2f2fc58fee9a20f231e197e5636070d3bb03
Antonin Kral <a.kral@bobek.cz>
2022-07-27 15:12:48 +0200

    Updating to latest greatest

--- bin/generate_banner.rb
+++ bin/generate_banner.rb
@@ -7,7 +7,7 @@ dst = ARGV[0]
 week = ''
 
 if dst =~ %r{long-reads/\d+/([\d\-]+)\.png}
-  parsed = $1
+  parsed = Regexp.last_match(1)
   week = parsed.sub('-', '/')
   dst = dst.sub('.png', '')
 else
@@ -20,22 +20,22 @@ pp week
 
 background = Magick::Pixel.from_hsla(rand(0.0..360.0), rand(0.0..100.0), 100)
 
-canvas = Magick::Image.new(3000, 3000) do
-  self.background_color = background
-  self.gravity = Magick::CenterGravity
+canvas = Magick::Image.new(3000, 3000) do |img|
+  img.background_color = background
+  img.gravity = Magick::CenterGravity
 end
 
-canvas_small = Magick::Image.new(3000, 1000) do
-  self.background_color = background
-  self.gravity = Magick::CenterGravity
+canvas_small = Magick::Image.new(3000, 1000) do |img|
+  img.background_color = background
+  img.gravity = Magick::CenterGravity
 end
 
-gc = Magick::Draw.new do
-  self.gravity = Magick::CenterGravity
-  self.pointsize = 250
-  self.undercolor = background
-  self.fill = 'white'
-  self.font = 'Courier-BoldOblique'
+gc = Magick::Draw.new do |d|
+  d.gravity = Magick::CenterGravity
+  d.pointsize = 250
+  d.undercolor = background
+  d.fill = 'white'
+  d.font = 'Courier-BoldOblique'
 end
 
 canvas.annotate(gc, 0, 0, 0, 0, "Weekend Long Reads\n\n#{week}")
