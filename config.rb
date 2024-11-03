e0ec0b99fd2f4449e4198b941435fc6c8ef071af
Antonin Kral <a.kral@bobek.cz>
2024-11-03 13:29:51 +0100

    Removal of long-reads

--- config.rb
+++ config.rb
@@ -32,27 +32,6 @@ activate :blog do |blog|
 end
 
 activate :blog do |blog|
-  blog.name = "long-reads"
-  blog.prefix = "long-reads"
-  blog.permalink = "{title}.html"
-  blog.sources = "{year}/{title}.html"
-  blog.layout = "layouts/post"
-  # blog.tag_template = "tag.html"
-  # blog.taglink = "{tag}/index.html"
-  # blog.calendar_template = "calendar.html"
-  blog.default_extension = ".md"
-
-  # Enable pagination
-  blog.paginate = true
-  blog.per_page = 12
-  blog.page_link = "page/{num}"
-
-  blog.generate_day_pages = false
-  blog.generate_month_pages = false
-  blog.generate_year_pages = false
-end
-
-activate :blog do |blog|
   blog.name = "til"
   blog.prefix = "til"
   blog.permalink = "{title}.html"
